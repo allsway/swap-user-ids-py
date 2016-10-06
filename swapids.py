1865034e1aefcc77267206b5e64e20f8d13d4b72
Sarina <ssinick@calstate.edu>
2016-10-05 17:33:24 -0700

    Updated swapids.py

--- swapids.py
+++ swapids.py
@@ -43,32 +43,35 @@ total_patrons = sys.argv[2] # always 5000
 for i in range(0,int(total_patrons)):
 	if i % limit == 0: # Now we want to increment i by 100
 		i += limit
-	
 	url = baseurl + '/almaws/v1/users?apikey=' + apikey + '&limit=' + str(limit) + '&offset=' + str(offset) # get batch of 100 users 
 	response = requests.get(url)
 	users = ET.fromstring(response.content)
 	for user in users:
+		swap = False
 		#primary_id = user.find('primary_id').text
-		primary_id = 'casper'
+		primary_id = 'bluford'
 		if len(primary_id) > 0  and not re.search('[ ;%&$#]', primary_id):
 			user_url = baseurl + '/almaws/v1/users/' + primary_id + '?apikey=' + apikey;
 			print user_url
 			response = requests.get(user_url);
 			patron = ET.fromstring(response.content)
 			for ids in patron.findall("user_identifiers"):
-				# add check to see if useridentifiers is set
-				# add check to see if there is only on ID with ID type of id_type_to_swap
-				for id in ids:
-					if id.find('id_type').text == id_type_to_swap:
-						new_primary = id.find('value').text
-						ids.remove(id)
-						swap = True
-						print (ET.tostring(ids))
-			if swap:
+				# only swap when there is one possible ID of the swap type
+				count = len(ids.findall("./user_identifier/[id_type='"+id_type_to_swap+"']"))
+				print count
+				if ids is not None and count == 1: 
+					for id in ids:
+						if id.find('id_type').text == id_type_to_swap:
+							new_primary = id.find('value').text
+							print new_primary
+							ids.remove(id)
+							swap = True
+							print (ET.tostring(ids))
+			if swap == True:
 				#put
 				headers = {"Content-Type": "application/xml"}
 				r = requests.put(user_url,data=ET.tostring(patron),headers=headers)
-			#	print r.content
+				#print r.content
 				logging.info('Removed old id:' + primary_id + ', new primary: ' + new_primary)
 				#get
 				response = requests.get(user_url)
@@ -76,18 +79,18 @@ for i in range(0,int(total_patrons)):
 				updated_user.find('primary_id').text = new_primary
 				#put
 				r = requests.put(user_url,data=ET.tostring(updated_user),headers=headers)
-			#	print r.content
 				
 				#get 
 				new_url = baseurl + '/almaws/v1/users/' + new_primary + '?apikey=' + apikey;
 				response = requests.get(new_url)
 				final_user = ET.fromstring(response.content)
 				final = addidentifier(final_user,primary_id,new_id_type)
-
+				
 				print ET.tostring(final)
+				r = requests.put(new_url,data=ET.tostring(final),headers=headers)
+				print r.content
 				logging.info('Successful id swap for old id:' + primary_id + ', new primary: ' + new_primary)
 
-				#get
 			
 				
 
