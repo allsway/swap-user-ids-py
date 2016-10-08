ffddaa0e11e6232f6a08373fa1599dd79428c072
Sarina <ssinick@calstate.edu>
2016-10-07 17:00:19 -0700

    Update swapids.py

--- swapids.py
+++ swapids.py
@@ -59,51 +59,51 @@ for i in range(0,int(total_patrons)):
 	if rstatus == 200:
 		users = ET.fromstring(response.content)
 		for user in users:
-		swap = False
-		primary_id = user.find('primary_id').text
+			swap = False
+			primary_id = user.find('primary_id').text
 
-		if len(primary_id) > 0  and not re.search('[ ;%&$#]', primary_id):
-			user_url = baseurl + '/almaws/v1/users/' + primary_id + '?apikey=' + apikey;
-			print user_url
-			response = requests.get(user_url);
-			patron = ET.fromstring(response.content)
-			for ids in patron.findall("user_identifiers"):
-				# only swap when there is one possible ID of the swap type
-				count = len(ids.findall("./user_identifier/[id_type='"+id_type_to_swap+"']"))
-				if ids is not None and count == 1: 
-					for id in ids:
-						if id.find('id_type').text == id_type_to_swap:
-							new_primary = id.find('value').text
-							print new_primary
-							ids.remove(id)
-							swap = True
-			if swap == True:
-				#put
-				headers = {"Content-Type": "application/xml"}
-				patron = removeroles(patron)
-				r = requests.put(user_url,data=ET.tostring(patron),headers=headers)
-				if r.status_code == 200:
-					# update primary ID
-					response = requests.get(user_url)
-					updated_user = ET.fromstring(response.content)
-					updated_user.find('primary_id').text = new_primary
+			if len(primary_id) > 0  and not re.search('[ ;%&$#]', primary_id):
+				user_url = baseurl + '/almaws/v1/users/' + primary_id + '?apikey=' + apikey;
+				print user_url
+				response = requests.get(user_url);
+				patron = ET.fromstring(response.content)
+				for ids in patron.findall("user_identifiers"):
+					# only swap when there is one possible ID of the swap type
+					count = len(ids.findall("./user_identifier/[id_type='"+id_type_to_swap+"']"))
+					if ids is not None and count == 1: 
+						for id in ids:
+							if id.find('id_type').text == id_type_to_swap:
+								new_primary = id.find('value').text
+								print new_primary
+								ids.remove(id)
+								swap = True
+				if swap == True:
 					#put
-					updated_user = removeroles(updated_user)
-					r = requests.put(user_url,data=ET.tostring(updated_user),headers=headers)
+					headers = {"Content-Type": "application/xml"}
+					patron = removeroles(patron)
+					r = requests.put(user_url,data=ET.tostring(patron),headers=headers)
 					if r.status_code == 200:
-						# Add new additional ID value 
-						new_url = baseurl + '/almaws/v1/users/' + new_primary + '?apikey=' + apikey;
-						response = requests.get(new_url)
-						final_user = ET.fromstring(response.content)
-						final = addidentifier(final_user,primary_id,new_id_type)
-						final = removeroles(final)
-						r = requests.put(new_url,data=ET.tostring(final),headers=headers)
-						logging.info('Successful id swap for old id:' + primary_id + ', new primary: ' + new_primary)
+						# update primary ID
+						response = requests.get(user_url)
+						updated_user = ET.fromstring(response.content)
+						updated_user.find('primary_id').text = new_primary
+						#put
+						updated_user = removeroles(updated_user)
+						r = requests.put(user_url,data=ET.tostring(updated_user),headers=headers)
+						if r.status_code == 200:
+							# Add new additional ID value 
+							new_url = baseurl + '/almaws/v1/users/' + new_primary + '?apikey=' + apikey;
+							response = requests.get(new_url)
+							final_user = ET.fromstring(response.content)
+							final = addidentifier(final_user,primary_id,new_id_type)
+							final = removeroles(final)
+							r = requests.put(new_url,data=ET.tostring(final),headers=headers)
+							logging.info('Successful id swap for old id:' + primary_id + ', new primary: ' + new_primary)
+						else:
+							logging.info('Failed to replace primary id for:' + primary_id + ', new id:' + new_primary)
+							logging.info(r.content)
 					else:
-						logging.info('Failed to replace primary id for:' + primary_id + ', new id:' + new_primary)
 						logging.info(r.content)
-				else:
-					logging.info(r.content)
 	offset += limit
 	
 
